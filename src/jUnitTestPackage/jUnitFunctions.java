3cded3c3a2f0d0458fa5233548faef23c285b757
VANSH <VANSH@LAPTOP-P4JM2548>
2022-04-23 21:06:15 +0530

    jUnitTesting using testAddNumbers, testAddStrings

---
+++ src/jUnitTestPackage/jUnitFunctions.java
@@ -0,0 +1,16 @@
+package jUnitTestPackage;
+ 
+
+public class jUnitFunctions {
+	
+	public static String addStrings(String x, String y){  
+		String str = x.concat(y);   
+		return str;
+	}
+	
+	public static int addNumbers(int a, int b){  
+		int sum = a + b;  
+		return sum;  
+	} 
+
+}
